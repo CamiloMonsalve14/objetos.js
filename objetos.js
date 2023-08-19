f7f7a906ef8d365b743ff283e442e21f3b5ea8c3
CamiloMonsalve14 <monsalvemaderac@gmail.com>
2023-08-19 09:37:03 -0500

    objetos javascript

---
+++ objetos.js
@@ -0,0 +1,15 @@
+
+let gato = {
+    numeroPatas : 4,
+    color : "blancoYnegro", 
+    camina: true,
+    raza: "Balinés",
+    edad: 2,
+    vuela: false,
+    tipo: "mamifero",
+    saludar: function saludar() {
+        console.log("meoow");
+    }
+}
+console.log(gato.raza);
+gato.saludar ()
